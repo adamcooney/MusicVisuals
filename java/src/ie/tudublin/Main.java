4def68d413b0eb5f1b13d024d3f78e6747dc0175
adamcooney <56026401+adamcooney@users.noreply.github.com>
2020-05-03 15:38:54 +0100

    fixing the fix

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,14 +1,8 @@
 package ie.tudublin;
 
-<<<<<<< HEAD
 import example.*;
 
 import c18372086.*;
-=======
-import example.CubeVisual;
-import example.MyVisual;
-import example.RotatingAudioBands;
->>>>>>> cc0250fe11bfd31233735e224c9253c06ad1e5b1
 
 public class Main
 {	
@@ -16,7 +10,6 @@ public class Main
 	public void startUI()
 	{
 		String[] a = {"MAIN"};
-<<<<<<< HEAD
         processing.core.PApplet.runSketch( a, new CubeVisual1());		
 	}
 	
@@ -24,14 +17,17 @@ public class Main
 	{
 		String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new MyMusicVisual());		
-=======
-        processing.core.PApplet.runSketch( a, new RotatingAudioBands());		
->>>>>>> cc0250fe11bfd31233735e224c9253c06ad1e5b1
 	}
-
+	
+	public void RotatingAudioBands()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new RotatingAudioBands());		
+	}
+	
 	public static void main(String[] args)
 	{
 		Main main = new Main();
-		main.MyMusicVisual();	
+		main.RotatingAudioBands();	
 	}
 }
